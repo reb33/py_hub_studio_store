be3214765e48391d10402d203e20118da1ade488
k.barylnikov <ipt_git_bot@ipt.ru>
2024-06-08 18:45:36 +0300

    product filter, filter with pagination, tmptag change query, url encode

--- goods/views.py
+++ goods/views.py
@@ -1,17 +1,24 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
 
 
 def catalog(request, category_slug):
 
-    page = request.GET.get('page', 1)
+    page = request.GET.get("page", 1)
+    on_sale = request.GET.get("on_sale", None)
+    order_by = request.GET.get("order_by", None)
 
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category_id__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(page)
