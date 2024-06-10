a754d2c41c239cfa6de831cc82a5d18a41a4a0e3
k.barylnikov <ipt_git_bot@ipt.ru>
2024-06-10 19:45:53 +0300

    postgres db connected and full search code

--- goods/utils.py
+++ goods/utils.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.db.models import Q
 
 from goods.models import Products
@@ -7,10 +8,15 @@ def q_search(query: str):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [w for w in query.split() if len(w) > 2]
-    q_objects = Q()
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # поиск в sqlite3
+    # keywords = [w for w in query.split() if len(w) > 2]
+    # q_objects = Q()
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
 
-    return Products.objects.filter(q_objects)
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
